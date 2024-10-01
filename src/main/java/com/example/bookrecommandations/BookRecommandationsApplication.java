eeb65fd27dcbce02a8333d3af57af7bd055939cc
seok <ox2789@naver.com>
2024-10-01 22:59:19 +0900

    first_test

---
+++ src/main/java/com/example/bookrecommandations/BookRecommandationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookrecommandations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommandationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommandationsApplication.class, args);
+	}
+
+}
